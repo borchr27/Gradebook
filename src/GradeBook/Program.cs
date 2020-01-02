0bc7c3d79865caab01dd2bf00cf9fc7564ca3ef7
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-01-02 13:33:13 -0500

    First commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        public int Length { get; }
+        static void Main(string[] args)
+        {
+            var book = new Book("Scotts GradeBook");
+            book.AddGrade(98);
+            book.AddGrade(90.5);
+
+            var result = 0.0; 
+            var grades = new List<double>() {82.6, 60.3, 104};
+            grades.Add(56.1);
+
+            foreach (var val in grades)
+            {
+                result += val;
+            } 
+            result /= grades.Count;
+            Console.WriteLine(result);           
+        }
+    }
+}
