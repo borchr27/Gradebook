715c9c296fd3a20cac9bf38bc67d3838eeb408a9
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-01-27 21:04:07 -0500

    Working thru tut

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,23 +12,10 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Scotts GradeBook");
-            book.AddGrade(98);
+            book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-
-            var result = 0.0; 
-            var grades = new List<double>() {82.6, 60.3, 104};
-            grades.Add(56.1);
-
-            var highGrade = double.MinValue;
-
-            foreach (var val in grades)
-            {
-                highGrade = Math.Max(val, highGrade);
-                result += val;
-            } 
-            result /= grades.Count;
-            Console.WriteLine("Average Grade: " + result);           
+            book.ShowStats();        
         }
     }
 }
